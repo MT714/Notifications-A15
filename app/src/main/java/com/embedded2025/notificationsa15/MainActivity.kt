0be1d3772b626f9e5dbd1abe9f100117c8a508f6
= <=>
2025-06-03 02:00:38 +0200

    layout base

--- app/src/main/java/com/embedded2025/notificationsa15/MainActivity.kt
+++ app/src/main/java/com/embedded2025/notificationsa15/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
