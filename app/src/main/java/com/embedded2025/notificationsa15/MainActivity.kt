d926fb758649448de3967a07fc44c0c01cf5a594
albepenz <151561592+albepenz@users.noreply.github.com>
2025-06-03 15:37:56 +0200

    Iniziato SimpleNotificationFragment

--- app/src/main/java/com/embedded2025/notificationsa15/MainActivity.kt
+++ app/src/main/java/com/embedded2025/notificationsa15/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.embedded2025.notificationsa15
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -14,7 +23,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.appbar.MaterialToolbar
-import kotlin.math.log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +41,29 @@ class MainActivity : AppCompatActivity() {
         toolbar.setupWithNavController(navController, appBarConfiguration)
         val navView = findViewById<NavigationView>(R.id.nav_view)
         NavigationUI.setupWithNavController(navView, navController)
+
+        askNotificationPermission()
+        createNotificationChannel()
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Log.d("PERMISSION", "Permesso notifiche concesso")
+        } else {
+            Log.d("PERMISSION", "Permesso notifiche negato")
+        }
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -43,5 +75,30 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return item.onNavDestinationSelected(navController)
                 || super.onOptionsItemSelected(item)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray)
+    {
+        //verifico che l'utente abbia dato il permesso per le notifiche
+        val p = grantResults[0] == PermissionChecker.PERMISSION_GRANTED
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.i(TAG, "Notification runtime permission granted: $p")
+    }
+
+    private fun createNotificationChannel(){
+        val name = "Notification"
+        val descriptionText = "Canale per notifiche semplici"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel("default_channel", name, importance).apply {
+            description = descriptionText
+        }
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object{
+        private val TAG = MainActivity::class.simpleName
     }
 }
--- app/src/main/java/com/embedded2025/notificationsa15/SimpleNotificationFragment.kt
+++ app/src/main/java/com/embedded2025/notificationsa15/SimpleNotificationFragment.kt
@@ -1,10 +1,17 @@
 package com.embedded2025.notificationsa15
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
 class SimpleNotificationFragment : Fragment() {
 
@@ -12,7 +19,36 @@ class SimpleNotificationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple_notification, container, false)
+        val view = inflater.inflate(R.layout.fragment_simple_notification, container, false)
+
+        val button = view.findViewById<Button>(R.id.btnSimpleNotification)
+        button.setOnClickListener {
+
+            showSimpleNotification(requireContext())
+        }
+
+        return view
     }
+
+    private fun showSimpleNotification(context: Context){
+        val builder = NotificationCompat.Builder(context, "default_channel")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(context.getString(R.string.simple_notification_title))
+            .setContentText(context.getString(R.string.simple_notification_text))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU ||
+            ContextCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED){
+
+            with(NotificationManagerCompat.from(context)) {
+                notify(1, builder.build())
+            }
+
+        }
+
+    }
+
 }
