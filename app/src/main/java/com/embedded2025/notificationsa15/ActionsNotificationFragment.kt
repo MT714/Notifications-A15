f0329e156a1ea3560ff5fb85e3078ad8111f60e0
matic <maticeres9@gmail.com>
2025-06-04 17:28:58 +0200

    Feat: Implementata la notifica con azioni e relativo BroadcastReceiver
    Implementata la funzionalità per mostrare notifiche con azioni (Archivia e Più tardi). Aggiunto un `BroadcastReceiver` per gestire le azioni delle notifiche e visualizzare un Toast corrispondente.
    
    Modifiche principali:
    - Aggiunto `ActionsNotificationFragment` per la demo delle notifiche con azioni.
    - Definito un nuovo layout `fragment_actions_notification.xml`.
    - Aggiornato `NotificationsHelper.kt` per:
        - Inizializzare `NotificationManagerCompat` e creare i canali di notifica (default e alta priorità).
        - Aggiungere metodi per creare `PendingIntent` (per Activity e Broadcast).
        - Implementare `showNotificationWithActions()` per mostrare la notifica con azioni.
        - Definire `NotificationActionReceiver` per gestire i click sulle azioni delle notifiche.
    - Registrato `NotificationActionReceiver` in `AndroidManifest.xml`.
    - Aggiunte stringhe per i nuovi canali di notifica, il titolo e la descrizione della notifica con azioni.
    - Aggiornato `MainActivity.kt` per inizializzare `NotificationsHelper`.
    - Aggiunta la voce "Actions notification" al menu di navigazione e al grafo di navigazione.

---
+++ app/src/main/java/com/embedded2025/notificationsa15/ActionsNotificationFragment.kt
@@ -0,0 +1,20 @@
+package com.embedded2025.notificationsa15
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+
+class ActionsNotificationFragment : Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_actions_notification, container, false).apply {
+            findViewById<Button>(R.id.btnShowNotification).setOnClickListener {
+                NotificationsHelper.showNotificationWithActions()
+            }
+        }
+}
--- app/src/main/java/com/embedded2025/notificationsa15/MainActivity.kt
+++ app/src/main/java/com/embedded2025/notificationsa15/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        NotificationsHelper.initialize(applicationContext)
         setupNavigation()
         setupNotifications()
     }
